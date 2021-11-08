f7f4ebf417a008864871917084ac31871c8332f7
Eliandro <eliandro.gomes42@gmail.com>
2021-11-08 20:21:01 -0300

    Novos comandos foram adicionados ao comando !help

--- commands/help.py
+++ commands/help.py
@@ -13,11 +13,16 @@ class Help(commands.Cog):
         embed.set_author(name='Lista de comandos do Bot')
         embed.add_field(name='join', value='Chama o Bot para o seu Canal de Voz', inline=False)
         embed.add_field(name='leave', value='Disconecta o bot do canal de voz.', inline=False)
-        embed.add_field(name='play', value='Toca músicas', inline=False)
+        embed.add_field(name='play [nome da música ou URL]', value='Toca a música inserida, caso uma já esteja tocando '
+                                                                   'a inserida é adicionada a fila', inline=False)
+        embed.add_field(name='queue', value='Exibe a música que está tocando e a fila', inline=False)
+        embed.add_field(name='clear', value='Remove todas as músicas da fila', inline=False)
+        embed.add_field(name='remove [posição da música]', value='Remove uma música específica da fila', inline=False)
         embed.add_field(name='skip', value='Pula para a próxima música caso exista uma na fila.', inline=False)
+        embed.add_field(name='skipto [posição da música]', value='Pula para a música desejada na fila', inline=False)
         embed.add_field(name='pause', value='Pausa a música que está tocando.', inline=False)
         embed.add_field(name='resume', value='Retorna a música que estava tocando.', inline=False)
-        embed.add_field(name='stop', value='Para a música que está tocando e remove o Bot do canal de voz'
+        embed.add_field(name='stop', value='Disconecta o Bot do canal de voz e limpa a fila'
                         , inline=False)
 
         await ctx.send(embed=embed)
